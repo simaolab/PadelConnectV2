fe2abcaa933e36bf74549ef199997d0440620171
Simão Correia <simao171999@gmail.com>
2024-11-28 21:10:20 +0000

    Seeders Fields and Companies & Promotion Client

--- Padel Project/BackEnd/Laravel_New/PadelProjectV1/database/seeders/DatabaseSeeder.php
+++ Padel Project/BackEnd/Laravel_New/PadelProjectV1/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(NationalitySeeder::class);
         $this->call(PaymentMethodSeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(FieldSeeder::class);
 
         User::factory()->create(
             [
@@ -26,6 +29,14 @@ class DatabaseSeeder extends Seeder
                 'nif'       => 999999991,
                 'role_id'   => 1,
                 'password'  => Hash::make('Az123456!'),
+            ],
+        );
+
+        Client::factory()->create(
+            [
+                'user_id'           => 1,
+                'first_name'        => 'Norberto',
+                'last_name'         => 'Moreira',
             ],
         );
 
@@ -40,6 +51,14 @@ class DatabaseSeeder extends Seeder
             ],
         );
 
+        Client::factory()->create(
+            [
+                'user_id'           => 2,
+                'first_name'        => 'Gestor',
+                'last_name'         => 'Teste',
+            ],
+        );
+
         User::factory()->create(
             [
                 'username'  => 'Cliente',
@@ -48,6 +67,14 @@ class DatabaseSeeder extends Seeder
                 'nif'       => 999999993,
                 'role_id'   => 3,
                 'password'  => Hash::make('Az123456!'),
+            ],
+        );
+
+        Client::factory()->create(
+            [
+                'user_id'           => 3,
+                'first_name'        => 'Cliente',
+                'last_name'         => 'Teste',
             ],
         );
     }
